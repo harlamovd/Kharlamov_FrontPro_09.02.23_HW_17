16e11f58c1a195bbb918c0d86e59f4f7da6a9960
harlamovd <d.v.harlamov@gmail.com>
2023-02-09 19:44:46 +0200

    first commit

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
-let declaredVariable;  // замменил на let потому что ругался редактор, const должна иметь значение
-const someData = 99;
-const obj = {age: 33};
+function addition () {
+    let sum = 0;
+    return num => sum += num;
+}
+let counter = addition();
+console.log(counter(1))
+console.log(counter(3))
+console.log(counter(2))
+console.log(counter(4))
 
-typeof 37;  // Number
-typeof obj; // Object
-typeof true;  // Boolean
-typeof 3.14;  // Number
-typeof obj.name; // Undefined
-typeof 42;  // Number
-typeof "";  // String
-typeof "bla"; // String
-typeof Boolean(1);  //Boolean
-typeof !!1; //  Boolean
-typeof obj.age;  // Number
-typeof Infinity;  // Number  вначале не понял потом погуглил что это обозначает бесконечность в числовом выражении
-typeof NaN; // Number
-typeof (someData + " Wisen"); // String
-typeof Number("1"); // Number
-typeof Number("shoe");  // Number принудительно можно назначить тим, также можно сделать и Objectом
-typeof `template literal`;  // String
-typeof "1";  // String
-typeof typeof 1;  // String я так понял потому что результат от "функции" (typeof) відаеться стрингой.
-typeof false; // Boolean
-typeof String(1); // String
-typeof undefined;  // undefined
-typeof { a: 1 };  //Object
-typeof declaredVariable; // Undefined
-typeof [1, 2, 4];  //Object
-typeof null;  // Object
-typeof "John";  // String
-typeof ([] + '1'); // String
-typeof (1 + '1');  //  String
-typeof (10 * '2');  // Number
+
+/*
+Написати функцію, яка приймає 1 параметр. з тим, що передали перший раз і т. д. сумуючи ці числа
+Все це із замиканнями, наприклад:*/
